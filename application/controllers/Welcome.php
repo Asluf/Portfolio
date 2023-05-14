d2919dc502fce1f51bd223670f2d26c6d4235d88
Asluf <ahmedasluf@gmail.com>
2023-05-15 00:15:16 +0530

    added the database model

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,49 +4,34 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Welcome extends CI_Controller
 {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('home');
-	}
+  public function index()
+  {
+    $this->load->view('home');
+  }
 
-	public function sendData()
-	{
+  public function sendData()
+  {
+    $this->load->model('admin_model');
+    $res = $this->admin_model->insertData($this->input->post());
+    
+  }
+  public function downloadPdf()
+  {
+    require_once(APPPATH . 'helpers/tcpdf/tcpdf.php');
 
-		// var_dump($this->input->post());
-		echo json_encode($this->input->post());
-	}
-	public function downloadPdf()
-	{
-		require_once(APPPATH . 'helpers/tcpdf/tcpdf.php');
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    $pdf->setTitle('Curriculum Vitae');
+    $pdf->AddPage();
 
-		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-		$pdf->setTitle('Curriculum Vitae');
-		$pdf->AddPage();
+    // 		$html = <<<EOD
+    // <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+    // <i>This is the first example of TCPDF library.</i>
+    // <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
+    // <p>Please check the source code documentation and other examples for further information.</p>
+    // <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
+    // EOD;
 
-		// 		$html = <<<EOD
-		// <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-		// <i>This is the first example of TCPDF library.</i>
-		// <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-		// <p>Please check the source code documentation and other examples for further information.</p>
-		// <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
-		// EOD;
-
-		$html = "<h1>Welcome to <a href='http://www.tcpdf.org' style='text-decoration:none;background-color:#CC0000;color:black;'>&nbsp;<span style='color:black;'>TC</span><span style='color:white;'>PDF</span>&nbsp;</a>!</h1>
+    $html = "<h1>Welcome to <a href='http://www.tcpdf.org' style='text-decoration:none;background-color:#CC0000;color:black;'>&nbsp;<span style='color:black;'>TC</span><span style='color:white;'>PDF</span>&nbsp;</a>!</h1>
 <i>This is the first example of TCPDF library.</i>
 <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
 <p>Please check the source code documentation and other examples for further information.</p>
@@ -54,7 +39,7 @@ class Welcome extends CI_Controller
 
 
 
-		$w3 = <<<EOF
+    $w3 = <<<EOF
 <body>
     <!-- Page Container -->
    
@@ -171,8 +156,8 @@ class Welcome extends CI_Controller
 EOF;
 
 
-		$pdf->writeHTMLCell(0, 0, '', '', $w3, 0, 1, 0, true, '', true);
+    $pdf->writeHTMLCell(0, 0, '', '', $w3, 0, 1, 0, true, '', true);
 
-		$pdf->Output('example_001.pdf', 'I');
-	}
+    $pdf->Output('example_001.pdf', 'I');
+  }
 }
