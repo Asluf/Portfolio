6f7e32523806f09b05bac4517a91891d259449ac
Ishan Senanayaka <120593815+Mishan20@users.noreply.github.com>
2023-06-10 13:04:40 +0530

    Added functions

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,9 +3,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 { 
-  // Ishan start
   
-  // Ishan end
+  public function index()
+  {
+    $this->load->view('home');
+  }
+
+  public function sendData()
+  {
+    $this->load->model('admin_model');
+    $res = $this->admin_model->insertData($this->input->post());
+  }
+  public function chooseTem($x)
+  {
+    $data['id'] = $x;
+    $this->load->view('template', $data);
+  }
+  
 
   // Madhu start
  
